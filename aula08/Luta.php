daab02940e54d3e70403ae62380a1e3513529f7d
Ribeirodiegos <santos.diegoribeiro@gmail.com>
2022-10-05 12:23:08 -0300

    Agregação de Objetos
    Tratando a agregação de objeto em PHP.
    Obs. o codigo esta com erro, aguardando para correção.
    Obs2. a classe aqui feita foi usada para o exercicio da aula anterior.

---
+++ aula08/Luta.php
@@ -0,0 +1,96 @@
+<?php
+  require_once 'aula07/Lutador.php';
+
+  class Luta {
+    // DECLARAÇÃO DE ATRIBUTOS
+    private $desafiado;
+    private $desafiante;
+    private $rounds;
+    private $aprovada;
+
+    // METODOS PUBLICOS
+    public function marcarLuta($l1, $l2)
+    {
+      if ($l1->getCategoria() === $l2->getCategoria() && ($l1 != $l2)) {
+        $this->aprovada = true;
+        $this->desafiado = $l1;
+        $this->desafiate = $l2;
+      } else {
+        $this->aprovada = false;
+        $this->desafiado = null;
+        $this->desafiante = null;
+      }
+    }
+
+    public function lutar()
+    {
+      if ($this->aprovada) {
+        $this->desafiado->apresentar();
+        $this->desafiante->apresentar();
+        $vencedor = rand(0,2);
+        switch ($vencedor) {
+          case '0':
+            echo "<p>Empate!</p>";
+            $this->desafiado->empatarLuta();
+            $this->desafiante->empatarLuta();
+            break;
+
+          case '1':
+            echo "<p>". $this->desafiado->getNome() . " venceu!</p>";
+            $this->desafiado->ganharLuta();
+            $this->desafiante->perderLuta();
+            break;
+          
+          case '2':
+            echo "<p>" . $this->desafiante->getNome(). " venceu!</p>";
+            $this->desafiante->ganharLuta();
+            $this->desafiad->perderLuta();
+            break;
+        }
+      } else {
+        echo "Essa luta nao pode acontecer.";
+      }
+    }
+
+    // CONSTRUTORES, GETTERS E SETTERS
+    public function getDesafiado()
+    {
+      return $this->desafiado;
+    }
+
+    public function setDesafiado($desafiado)
+    {
+      $this->desafiado = $desafiado;
+    }
+
+    public function getDesafiante()
+    {
+      return $this->desafiante;
+    }
+
+    public function setDesafiante($desafiante)
+    {
+      $this->desafiante = $desafiante;
+    }
+
+    public function getRounds()
+    {
+      return $this->rounds;
+    }
+
+    public function setRounds($rounds)
+    {
+      $this->rounds = $rounds;
+    }
+
+    public function getAprovada()
+    {
+      return $this->aprovada;
+    }
+
+    public function setAprovada($aprovada)
+    {
+      $this->aprovada = $aprovada;
+    }
+  }
+?>
